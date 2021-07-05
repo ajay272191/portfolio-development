5414bc339167b37fc611f18398ced85b1f984ce0
ajay272191 <ajayrmavs33236@gmail.com>
2021-07-05 23:45:19 +0530

    Adding portfolio having navigation as sidebar

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// writing-text
+const Portfolio = function() {
+
+	function typeAnimation() {
+		Typed.new("#writing_text", {
+			strings: [
+				"  I am a developer from Lucknow, India.   ", "passionate about coding and love building things.   ", "worked on many technologies such as JavaScript, Python, HTML... ", "Please let me know if you have anythin ask, assign or share.               "
+			],
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				typeAnimation();
+			},
+		});
+	}
+	return {
+		typeAnimation: typeAnimation
+	}
+}();
+Portfolio.typeAnimation();
