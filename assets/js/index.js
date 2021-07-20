0fe253fc07120d134450d6bbe51f08f828606a97
ajay272191 <ajayrmavs33236@gmail.com>
2021-07-20 21:29:23 +0530

    Portfolio page added

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing_text", {
 			strings: [
-				"  I am a developer from Lucknow, India.   ", "passionate about coding and love building things.   ", "worked on many technologies such as JavaScript, Python, HTML... ", "Please let me know if you have anythin ask, assign or share.               "
+				"  I am a Freelance Developer from Lucknow, India.   ", "passionate about coding and love building things.   ", "worked on many technologies such as JavaScript, Python, HTML... ", "Please let me know if you have anythin ask, assign or share.               "
 			],
 			stringsElement: null,
 			// typing speed
