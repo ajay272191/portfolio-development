1311cbb4c7397b657c4965a6e0f1160655383423
ajay272191 <ajayrmavs33236@gmail.com>
2021-09-27 23:51:35 +0530

    making home page responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,12 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing_text", {
 			strings:[
-				"I'm a Freelance Developer from Lucknow, India.   ", "Please let me know if you have anything to ask, assign.  "
+				"I'm a Freelance Developer.   ", "I love building and sharing things.  ", "I'm available for work.", "Please let me know if you've anything", " ",
 				// "  I am a Freelance Developer from Lucknow, India.   ", "passionate about coding and love building things.   ", "worked on many technologies such as JavaScript, Python, HTML... ", "Please let me know if you have anythin ask, assign or share.               "
 			],
 			stringsElement: null,
 			// typing speed
-			typeSpeed: 1,
+			typeSpeed: 2,
 			contentType: 'text',
 			callback: function() {
 				typeAnimation();
